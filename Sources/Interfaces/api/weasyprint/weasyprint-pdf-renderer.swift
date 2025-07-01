ae8848ac88033cc99767f1817fb3e1541fa8bfbf
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-01 11:23:17 +0200

    encoding custom

--- Sources/Interfaces/api/weasyprint/weasyprint-pdf-renderer.swift
+++ Sources/Interfaces/api/weasyprint/weasyprint-pdf-renderer.swift
@@ -7,11 +7,14 @@ public protocol PDFRenderable {
 
 public struct WeasyPrintRenderer: PDFRenderable {
     private let weasyBinaryPath: String
+    public let encoding: String.Encoding
     
     public init(
         weasyBinaryPath: String = "/opt/homebrew/bin/weasyprint",
+        encoding: String.Encoding = .utf8
     ) {
         self.weasyBinaryPath = weasyBinaryPath
+        self.encoding = encoding
     }
     
     public func pdf(html: URL, css: URL, destination: String) throws {
@@ -46,20 +49,20 @@ public struct WeasyPrintRenderer: PDFRenderable {
         let status = process.terminationStatus
         if status != 0 {
             let data = pipe.fileHandleForReading.readDataToEndOfFile()
-            let output = String(data: data, encoding: .utf8) ?? "<no output>"
+            let output = String(data: data, encoding: encoding) ?? "<no output>"
             throw PDFGenerationError.processFailed(exitCode: status, output: output)
         }
     }
 }
 
 extension String {
-    public func weasyPDF(css: CSSPageSetting = CSSPageSetting(), destination: String) throws {
-        let htmlTemp = try self.tempFile(fileExtension: "html")
+    public func weasyPDF(css: CSSPageSetting = CSSPageSetting(), destination: String, encoding: String.Encoding = .utf8) throws {
+        let htmlTemp = try self.tempFile(fileExtension: "html", encoding: encoding)
 
         let cssString = css.css()
         let cssTemp = try cssString.tempFile(fileExtension: "css")
 
-        let renderer = WeasyPrintRenderer()
+        let renderer = WeasyPrintRenderer(encoding: encoding)
         try renderer.pdf(html: htmlTemp, css: cssTemp, destination: destination)
     }
 }
