8591eb69d35328490c913d80c521564eeb9dba97
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-20 10:29:02 +0200

    standard assets

---
+++ Sources/Interfaces/api/html/html-standard-assets.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+enum HTMLStandardAssets {
+    static let navigationIcon: String = """
+        <svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\">
+            <circle cx=\"12\" cy=\"12\" r=\"12\"/>
+            <text x=\"12\" y=\"17\" font-size=\"14\" font-weight=\"bold\" text-anchor=\"middle\" fill=\"#fff\">i</text>
+        </svg>
+    """
+}
