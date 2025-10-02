23e4a9daae18e62461e52f6b241826536b953c0a
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-10-02 11:43:37 +0200

    rename

--- Sources/Interfaces/api/json-pretty.swift
+++ Sources/Interfaces/api/json-pretty.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public enum JSONPretty {
-    public static func prettyString(from data: Data) -> String? {
+    public static func string(from data: Data) -> String? {
         do {
             let obj = try JSONSerialization.jsonObject(with: data, options: [])
             let pretty = try JSONSerialization.data(withJSONObject: obj, options: [.prettyPrinted, .withoutEscapingSlashes])
