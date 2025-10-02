db5f7f4399d6a2709451bbe89cbd151efadf46b7
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-10-02 11:55:09 +0200

    make sendable

--- Sources/Interfaces/api/pdok/pdok-models.swift
+++ Sources/Interfaces/api/pdok/pdok-models.swift
@@ -1,15 +1,15 @@
 import Foundation
 
-public struct PDOKSuggestResponse: Codable {
+public struct PDOKSuggestResponse: Codable, Sendable {
     public let response: PDOKResponse
 }
 
-public struct PDOKResponse: Codable {
+public struct PDOKResponse: Codable, Sendable {
     public let numFound: Int?
     public let docs: [PDOKDoc]?
 }
 
-public struct PDOKDoc: Codable {
+public struct PDOKDoc: Codable, Sendable {
     public let weergavenaam: String?
     // extensible
 }
